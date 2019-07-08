a0e151f806142f969c076cac2c51f835786c55e5
西里 <273294806@qq.com>
2019-07-08 10:59:27 +0800

    'test'

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,28 +1,38 @@
 <template>
-    <div class="login_container">
-        <div class="login_box">
-            <!-- 头像 -->
-            <div class="avatar_box">
-                <img src="../assets/logo.png" alt="">
-            </div>
-            <!-- 登录表单 -->
-            <el-form :model="loginForm" ref="LoginFormRef" :rules="loginFormRules" label-width="0px" class="login_form">
-                <!-- 用户名 -->
-                <el-form-item prop="username">
-                    <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user" ></el-input>
-                </el-form-item> 
-                <!-- 密码 -->
-                <el-form-item prop="password">
-                    <el-input type="password" v-model="loginForm.password" prefix-icon="iconfont icon-3702mima"></el-input>
-                </el-form-item> 
-                <!-- 按钮 -->
-                <el-form-item class="btns">
-                    <el-button type="primary" @click="login">登录</el-button>
-                    <el-button type="info" @click="resetLoginForm">重置</el-button>
-                </el-form-item> 
-            </el-form>
-        </div>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt />
+      </div>
+      <!-- 登录表单 -->
+      <el-form
+        :model="loginForm"
+        ref="LoginFormRef"
+        :rules="loginFormRules"
+        label-width="0px"
+        class="login_form"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            type="password"
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+  </div>
 </template>
 
 <script>
@@ -50,7 +60,7 @@ export default {
           {
             min: 6,
             max: 15,
-            message: '密码长度在 6 到 15 个字符',
+            message: '密码长度在 6 到 150 个字符',
             trigger: 'blur'
           }
         ]
